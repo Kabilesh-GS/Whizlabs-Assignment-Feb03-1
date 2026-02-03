d2d4182f63900ae8e22045ebb12b69e8a394ee82
Kabilesh-GS <kabileshgs@gmail.com>
2026-02-03 12:18:51 +0530

    added localstorage to storesearched data

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const userUI = document.getElementById('User-Container');
 let data;
+const searchInput = document.getElementById('inputSearch');
+searchInput.value = localStorage.getItem('search');
 
 async function fetchData(){
   const res = await fetch('https://santhosh1203122.github.io/users_data/users.json');
   data = await res.json();
   localStorage.setItem('users', JSON.stringify(data));
-  //createUser(data);
 }
-//fetchData();
 data = JSON.parse(localStorage.getItem('users'));
-createUser(data);
+const firstTwenty = data.slice(0, 20);
+createUser(firstTwenty);
 
 function searchUser(d){
+  localStorage.setItem('search', d);
   setTimeout(() => {
+    if(d.length === 0){
+      userUI.innerHTML = '';
+      createUser(firstTwenty);
+      return;
+    }
     const search = data.filter((e) => (e.login.toLowerCase().includes(d)));
     userUI.innerHTML = '';
     createUser(search);
@@ -24,13 +31,13 @@ function createUser(data){
   data.map(e => (
     userUI.innerHTML +=
     `
-      <article id="user-list">
+      <article class="user-Box">
         <section>
           <img alt="User Profile" src=${e.avatar_url}/> 
         </section>
         <section>
           <h2>${e.login}</h2>
-          <p>Profile URL: <a href=${e.html_url} target="_blank">Link</a></p>
+          <p><a href=${e.html_url} target="_blank">Link</a></p>
         </section>
       </article>
     `
